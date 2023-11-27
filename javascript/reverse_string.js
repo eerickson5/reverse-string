d6661d26d4ef35a1fade7fd90bf13e8ad0849ca0
Elise Erickson <elisece717@gmail.com>
2023-11-27 11:59:31 -0500

    satisfies tests

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,23 @@
 function reverseString(str) {
-  // type your code here
+  if(typeof str === "string"){
+    let lastIndex = str.length - 1;
+    let newStr = str.substring(lastIndex);
+    for(let i = lastIndex - 1; i >= 0; i--){
+      newStr = newStr + str.substring(i, i+1);
+    }
+    return(newStr);
+  } else {
+    return undefined
+  }
+  
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: ' j2 ugs4'");
+  console.log("=>", reverseString("4sgu 2j "))
+
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
